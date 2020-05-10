c6ce04204047ad6543d09878046e1d2db9072120
Kadir Niyazi AÇAR <kadirnacar@kadirnacar.com>
2020-05-11 01:44:01 +0300

    .

--- brekbrek/src/config.ts
+++ brekbrek/src/config.ts
@@ -1,6 +1,8 @@
 const config = {
-  restUrl: 'http://192.168.8.103:3001',
-  wsUrl: 'ws://192.168.8.103:3001',
+  // restUrl: 'http://192.168.8.103:3001',
+  // wsUrl: 'ws://192.168.8.103:3001',
+  restUrl: 'http://azure.kadirnacar.com:3001',
+  wsUrl: 'ws://azure.kadirnacar.com:3001',
   // googleWebApiKey: 'AIzaSyCXZY4NrDOtsqaSx4qDhbBwlTsAho228ps',
   googleWebClientId: '902480885959-odod12envften05tg38bvnurbcdddqmi.apps.googleusercontent.com',
 };
