0a313ab653c5cea0c92501f918c7680a6e4174bf
Kadir Niyazi <kadirnacar@kadirnacar.com>
2023-08-04 15:31:41 +0300

    Update config.ts

--- brekbrek/src/config.ts
+++ brekbrek/src/config.ts
@@ -1,7 +1,7 @@
 const config = {
   // restUrl: 'http://192.168.1.100:3001',
   // wsUrl: 'ws://192.168.1.100:3001',
-  securityKey: 'g539zXhYZ7ejh',
+  securityKey: '',
   restUrl: 'https://brekbrek.kadirnacar.com:3001',
   wsUrl: 'wss://brekbrek.kadirnacar.com:3001',
   googleWebClientId: '',
