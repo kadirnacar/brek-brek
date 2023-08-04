dbf5c1b064a75a907084c7415c87f58e8c6d1902
Kadir Niyazi <kadirnacar@kadirnacar.com>
2023-08-04 15:30:07 +0300

    Update config.ts

--- brekbrek/src/config.ts
+++ brekbrek/src/config.ts
@@ -3,8 +3,7 @@ const config = {
   // wsUrl: 'ws://192.168.8.103:3001',
   restUrl: 'http://azure.kadirnacar.com:3001',
   wsUrl: 'ws://azure.kadirnacar.com:3001',
-  // googleWebApiKey: 'AIzaSyCXZY4NrDOtsqaSx4qDhbBwlTsAho228ps',
-  googleWebClientId: '902480885959-odod12envften05tg38bvnurbcdddqmi.apps.googleusercontent.com',
+  googleWebClientId: '',
 };
 
 export default config;
